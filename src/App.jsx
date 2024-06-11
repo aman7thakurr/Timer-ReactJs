8e5582508d2345f3d0ceaefe255979d88e14a05b
Aman Thakur <amant8425@gmail.com>
2024-06-11 19:51:26 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import React,{useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-
-
-function App() { const [initialHour, setInitialHour] = useState(0);
+function App() {
+  const [initialHour, setInitialHour] = useState(0);
   const [initialMinute, setInitialMinute] = useState(0);
   const [initialSecond, setInitialSecond] = useState(0);
   const [currentHour, setCurrentHour] = useState(0);
@@ -66,69 +65,60 @@ function App() { const [initialHour, setInitialHour] = useState(0);
     setCurrentMinute(initialMinute);
     setCurrentSecond(initialSecond);
   };
- 
+
   return (
     <div className="App">
       <div className="c-timers">
-      
         <div className="timerContainer">
           <div className="countdownList">
             <div className="c-timer">
               <div className="timeCount">
                 <span className="timeLeft">  
-                {String(currentHour).padStart(2, '0')}:{String(currentMinute).padStart(2, '0')}:{String(currentSecond).padStart(2, '0')}
-      </span>
+                  {String(currentHour).padStart(2, '0')}:{String(currentMinute).padStart(2, '0')}:{String(currentSecond).padStart(2, '0')}
+                </span>
                 <div className="toolbox">
-                  {!isRunning &&<button type="button" className="c-timer__btn-start" onClick={startTimer}>Start</button>}
+                  {!isRunning && <button type="button" className="c-timer__btn-start" onClick={startTimer}>Start</button>}
                   {isRunning && <button type="button" className="c-timer__btn-pause" onClick={pauseTimer}>Pause</button>}
-                  <button type="button" className="c-timer__btn-reset" onClick={resetTimer}>Reset</button>
+                  <button type="button" className="c-timer__btn-reset" onClick={resetTimer} disabled={isRunning}>Reset</button>
                 </div>
               </div>
               <div className="c-timer__title">
                 <span className="tad-sortable-anchor">Timer 1 </span>
                 (<span className="tad-sortable-anchor">{String(currentHour).padStart(2, '0')}:{String(currentMinute).padStart(2, '0')}:{String(currentSecond).padStart(2, '0')}</span>)
                 <span className="small soft"></span>
-                
                 <span className="c-timer__edit-wrap">
-                 <span className='label'> 
-                  <label className='txt'>Hrs</label>
-                  <input type='number' className='ip'min={0}  value={initialHour}
-          onChange={(e) => setInitialHour(parseInt(e.target.value))}></input></span> 
-                 <span className='label'>
-                  <label className='txt'>Mins</label>
-                  <input type='number' className='ip' min={0}  value={initialMinute}
-          onChange={(e) => setInitialMinute(parseInt(e.target.value))}></input></span>
-                 <span className='label'>
-                  <label className='txt'>Sec</label>
-                  <input type='number' className='ip' min={0} max={60} value={initialSecond}
-          onChange={(e) => setInitialSecond(parseInt(e.target.value))}></input></span>  
+                  <span className='label'>
+                    <label className='txt'>Hrs</label>
+                    <input type='number' className='ip' min={0} value={initialHour} onChange={(e) => setInitialHour(parseInt(e.target.value))}></input>
+                  </span>
+                  <span className='label'>
+                    <label className='txt'>Mins</label>
+                    <input type='number' className='ip' min={0} value={initialMinute} onChange={(e) => setInitialMinute(parseInt(e.target.value))}></input>
+                  </span>
+                  <span className='label'>
+                    <label className='txt'>Sec</label>
+                    <input type='number' className='ip' min={0} max={60} value={initialSecond} onChange={(e) => setInitialSecond(parseInt(e.target.value))}></input>
+                  </span>
                   <button className="c-timer__edit"><i className="i-font"> </i> Edit</button>
                 </span>
               </div>
-             
             </div>
           </div>
-        </div> <div className="progressbar" title="Progress bar">
-                <div className="currentProgress currentProgress--start" >
-                  <span className="timeAfter"></span><br/>
-                </div>
-              </div>
-      
-        
-      </div>  
-      <div className="c-timer__footer">
-          <div className="six-columns">
-         
-    <i className="i-font-i-add"></i><br/>+ Add another timer
-
-
-          </div>
-          <div className="six-columns">
-            <div className="totalTimeLeft">Total: <span>{initialHour}:{initialMinute}:{initialSecond}</span></div>
+        </div>
+        <div className="progressbar" title="Progress bar">
+          <div className="currentProgress currentProgress--start">
+            <span className="timeAfter"></span><br/>
           </div>
         </div>
-        
-         
+      </div>
+      <div className="c-timer__footer">
+        <div className="six-columns">
+          <i className="i-font-i-add"></i><br/>+ Add another timer
+        </div>
+        <div className="six-columns">
+          <div className="totalTimeLeft">Total: <span>{initialHour}:{initialMinute}:{initialSecond}</span></div>
+        </div>
+      </div>
     </div>
   );
 }
